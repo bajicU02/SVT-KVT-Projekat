6ffcb4b469a84735778489512aa3e26f7ddeae52
Ugljesa Bajic <ugljesabajic2002@gmail.com>
2024-06-09 07:17:40 +0200

    Initial commit for frontend project

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [FormsModule]
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
