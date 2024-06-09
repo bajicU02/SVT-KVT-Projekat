50b3f8c35e1a8ba7ccdc1028c1a66f62c5aebdda
Ugljesa Bajic <ugljesabajic2002@gmail.com>
2024-06-09 07:28:27 +0200

    frontend start

---
+++ backend/src/main/java/com/example/svtkvtproject/SvtkvtprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.svtkvtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtkvtprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvtkvtprojectApplication.class, args);
+    }
+}
